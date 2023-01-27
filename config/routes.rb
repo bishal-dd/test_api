006b254cbfe16ded89e303bd3d81aef779c54b8e
Bishal Dhakal <dhakalbishal930@gmail.com>
2023-01-27 16:02:23 +0600

    frontend with react setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :friends
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+        resources :friends
+    end
+  end
+ 
 end

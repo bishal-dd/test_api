fb6bdb36f9b92f0847437c40408b3a517ffc5bcc
Bishal Dhakal <dhakalbishal930@gmail.com>
2023-01-30 21:34:57 +0600

    table added

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -2,17 +2,30 @@ import React from "react";
 
 function Home(props) {
   return (
-    <div>
-      {props.friends.map((friend) => {
-        return (
-          <div key={friend.id}>
-            <p>{friend.name}</p>
-            <p>{friend.age}</p>
-            <p>{friend.email}</p>
-          </div>
-        );
-      })}
-      sss
+    <div class="container mt-2">
+      <h1>My Friends</h1>
+      <table class="table table-bordered table-hover">
+        <thead class="thead-dark">
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Name</th>
+            <th scope="col">Age</th>
+            <th scope="col">Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.friends.map((friend) => {
+            return (
+              <tr key={friend.id}>
+                <th scope="row">{friend.id}</th>
+                <td>{friend.name}</td>
+                <td>{friend.age}</td>
+                <td>{friend.email}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
